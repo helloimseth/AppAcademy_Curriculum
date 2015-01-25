f84a7952890544b13746f100802f0444be13388d
Seth Hamlin <helloimseth@gmail.com>
2015-01-25 15:50:57 -0500

    Built run method as well as private helper methods including build_path and explore_current_words

--- wordchains.rb
+++ wordchains.rb
@@ -5,6 +5,33 @@ class WordChainer
     @dictionary = File.readlines(dictionary_file_name).map(&:chomp).to_set
   end
 
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = {source => nil}
+
+    until @current_words.empty?
+      new_current_words = explore_current_words(@current_words)
+      @current_words = new_current_words
+    end
+  end
+
+  private
+
+  def explore_current_words(current_words)
+    [].tap do |new_current_words|
+      @current_words.each do |word|
+        adjacent_words(word).each do |adj_word|
+          next if @all_seen_words.include?(adj_word)
+          new_current_words << adj_word
+          @all_seen_words[adj_word] = word
+        end
+      end
+    end.each do |word|
+      puts "#{word} came from #{@all_seen_words[word]}"
+    end
+
+  end
+
   def adjacent_words(word)
 
     [].tap do |adjacents|
@@ -16,14 +43,12 @@ class WordChainer
 
           next if letter == char
           test_word[index] = letter
-          
+
           adjacents << test_word if @dictionary.include?(test_word)
         end
-
       end
-
     end
-
   end
+
 
 end
