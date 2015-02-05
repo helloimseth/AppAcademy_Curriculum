38b79cbde449db70a97572eabdf3d901d4f0da4c
Seth Hamlin <helloimseth@gmail.com>
2015-02-05 10:57:26 -0500

    Finishes associations.

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,14 @@
 class Response < ActiveRecord::Base
-  validates :user_id, :question_id, presence: true
-  
+  validates :user_id, :answer_id, presence: true
+
+  has_many :answer_choices,
+    class_name: 'AnswerChoice',
+    foreign_key: :answer_id,
+    primary_key: :id
+
+  belongs_to :respondent,
+    class_name: 'User',
+    foreign_key: :user_id,
+    primary_key: :id
+
 end
