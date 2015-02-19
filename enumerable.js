d8670c2e9c7e5315d703fb5553da72a236e6345a
Seth Hamlin <helloimseth@gmail.com>
2015-02-19 12:25:49 -0500

    Implements myInject function on Array prototype

--- enumerable.js
+++ enumerable.js
@@ -1,15 +1,32 @@
-Array.prototype.my_each = function(funct) {
+Array.prototype.myEach = function(funct) {
   for (var i = 0; i < this.length; i++) {
     funct(this[i]);
   }
 };
 
 
-Array.prototype.my_map = function(funct) {
-  returnArray = [];
-  for (var i = 0; i < this.length; i++) {
-    returnArray.push(funct(this[i]));
-  }
+Array.prototype.myMap = function(funct) {
+  var returnArray = [];
+
+  var createMap = function(num){
+    returnArray.push(funct(num));
+  };
+
+  this.myEach(createMap);
 
   return returnArray;
+};
+
+Array.prototype.inject = function(funct) {
+  var start = this.shift();
+  var accum = start;
+
+  var createInjection = function(num){
+    if (accum = funct(accum, num));
+  };
+
+  this.myEach(createInjection);
+
+  this.unshift(start);
+  return accum;
 };
