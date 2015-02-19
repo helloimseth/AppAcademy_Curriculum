43fb97e351a8889eaa4a417645d6811d02422754
Seth Hamlin <helloimseth@gmail.com>
2015-02-19 11:21:29 -0500

    Implements twoSum function to Array prototype

--- arrays.js
+++ arrays.js
@@ -19,3 +19,18 @@ Array.prototype.includes = function (val) {
   });
   return returnValue;
 };
+
+
+Array.prototype.twoSum = function () {
+  var twoSums = [];
+
+  for (var i = 0; i < this.length; i++) {
+    for (var j = i; j < this.length; j++) {
+      if (j !== i && (this[i] + this[j]) === 0) {
+        var pair = [i, j]
+        twoSums.push(pair);
+    };
+  };
+
+  return twoSums;
+};
