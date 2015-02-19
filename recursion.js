9cd59ac8ab2f8cf01870631ca75297e080725bb8
Seth Hamlin <helloimseth@gmail.com>
2015-02-19 14:43:39 -0500

    Implement recursive exponent functions

--- recursion.js
+++ recursion.js
@@ -11,3 +11,25 @@ var range = function(start, end) {
     nextRange.push(end - 1);
     return nextRange;
 };
+
+var recursion1 = function (base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else {
+    return base * recursion1(base, exp - 1);
+  }
+}
+
+
+var recursion2 = function (base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else if (exp === 1) {
+    return base;
+  } else {
+    var newExp = (exp - exp % 2) / 2;
+    return recursion2(base, exp % 2) *
+           recursion2(base, newExp) *
+           recursion2(base, newExp);
+  }
+};
