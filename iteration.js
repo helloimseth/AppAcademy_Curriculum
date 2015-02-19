89467aae7849b9c35e59fd66a16916946daf12b8
Seth Hamlin <helloimseth@gmail.com>
2015-02-19 14:13:50 -0500

    Implements substrings function

--- iteration.js
+++ iteration.js
@@ -15,3 +15,29 @@ var bubbleSort = function (arr) {
 
   return arr
 };
+
+
+Array.prototype.includes = function (val) {
+  var returnValue = false;
+  this.forEach(function (el, i){
+    if (val === el) {
+      returnValue = true;
+    }
+  });
+  return returnValue;
+};
+
+var substrings = function (string) {
+  var substringsArr = [];
+
+  for (var i = 0; i < string.length; i++) {
+    for (var j = i + 1; j <= string.length; j++) {
+      var sub = string.substr(i, j - i);
+      if (!substringsArr.includes(sub)) {
+        substringsArr.push(sub);
+      }
+    };
+  };
+
+  return substringsArr;
+};
