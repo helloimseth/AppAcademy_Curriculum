d1a516526531a7085ff9443df197f49c9f700b42
Seth Hamlin <helloimseth@gmail.com>
2015-03-02 11:46:59 -0500

    Implement detail on RootView

--- pokedex-1-skeleton/app/assets/javascripts/pokedex-1B.js
+++ pokedex-1-skeleton/app/assets/javascripts/pokedex-1B.js
@@ -1,5 +1,25 @@
 Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
+  var $article = $('<article>').addClass('detail');
+  $article.append('<img src=' + pokemon.escape("image_url") + '>')
+  console.log(pokemon.attributes);
+  $ul = $("<ul>");
+  _.each(pokemon.attributes, function (val, key) {
+    if (key !== "image_url") {
+      $ul.append("<li>" + key + " - " + val + "</li>");
+    }
+  })
+  $article.append($ul);
+  this.$pokeDetail.append($article);
 };
 
 Pokedex.RootView.prototype.selectPokemonFromList = function (event) {
 };
+//
+//
+// { "id":1,
+//   "attack":125,
+//   "defense":100,
+//   "image_url":"/assets/pokemon_snaps/127.png",
+//   "moves":["vicegrip"],
+//   "name":"Pinsir",
+//   "poke_type":"bug" }
