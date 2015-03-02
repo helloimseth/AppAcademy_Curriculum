a7a2a076fc325fbebbb49aadec1af79965243d55
Seth Hamlin <helloimseth@gmail.com>
2015-03-02 11:28:57 -0500

    Implements RootView functions

--- pokedex-1-skeleton/app/assets/javascripts/pokedex-1A.js
+++ pokedex-1-skeleton/app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,18 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $li = $('<li>');
+
+  $li.text(pokemon.escape("name") + " - " + pokemon.escape("poke_type")).addClass('poke-list-item');
+  this.$pokeList.append($li);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function (callback) {
+  if(!callback) {
+    callback = this.addPokemonToList.bind(this);
+  }
+
+  this.pokes.fetch({
+    success: function (pokes) {
+      pokes.each(callback)
+    }
+  });
 };
