8d9d94be6cfb2f8c5f4ca506625f675b7cdecccf
Seth Hamlin <helloimseth@gmail.com>
2015-03-04 16:17:22 -0500

    Add new and edit routes for posts

--- app/assets/javascripts/views/postShow.js
+++ app/assets/javascripts/views/postShow.js
@@ -7,12 +7,22 @@ Journails.Views.PostShow = Backbone.View.extend({
     this.listenTo(this.model, "sync", this.render);
   },
 
+  events: {
+    "click .edit-post": "renderPostEdit"
+  },
+
   render: function () {
     var template = this.template({ post: this.model });
 
     this.$el.html(template);
 
     return this;
-  }
+  },
 
+  renderPostEdit: function(){
+    Backbone.history.navigate(
+      '/posts/' + this.model.id + '/edit',
+      { "trigger": true }
+    )
+  }
 });
--- app/assets/javascripts/views/postsIndex.js
+++ app/assets/javascripts/views/postsIndex.js
@@ -1,20 +1,35 @@
 Journails.Views.PostsIndex = Backbone.View.extend({
-  tagName: 'ul',
+  tagName: 'section',
+
+  template: JST['postsIndex'],
 
   initialize: function (options) {
     this.posts = options.posts
     this.listenTo(this.posts, "remove sync", this.render)
   },
 
+  events: {
+    "click .new-post": "renderPostNew"
+  },
+
   render: function () {
-    this.$el.empty();
+    var template = this.template();
+    this.$el.html(template);
 
     this.posts.each(function (post) {
       var postIndexItem = new Journails.Views.PostsIndexItem({ post: post });
 
-      this.$el.append(postIndexItem.render().$el);
+      this.$el.find('.post-list').append(postIndexItem.render().$el);
     }.bind(this));
 
+
     return this;
+  },
+
+  renderPostNew: function(){
+    Backbone.history.navigate(
+      '/posts/new',
+      { "trigger": true }
+    )
   }
 });
